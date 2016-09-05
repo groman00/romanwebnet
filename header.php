cde423a6812f70784bd007102145ab67ce5ed7f6
Roman, Greg <greg.roman@teamaol.com>
2016-09-04 22:16:12 -0400

    home page design updates

--- header.php
+++ header.php
@@ -6,25 +6,25 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <?php wp_head(); ?>
-<link href="https://fonts.googleapis.com/css?family=Ubuntu+Mono:400,700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,700" rel="stylesheet">
 <link href="<?php echo get_template_directory_uri(); ?>/dist/css/style.css" rel="stylesheet">
 </head>
     <body>
     <header id="header" class="header">
         <nav class="nav container">
             <a class="nav-item" href="/blog/" title="Blog">
-                <?php svg_icon('pencil'); ?>
+                <?php svg_icon('rss'); ?>
             </a>
-            <a class="nav-item" href="https://www.linkedin.com/in/gregory-roman-8486485" title="LinkedIn" target="_blank">
-                <?php svg_icon('linkedin'); ?>
-            </a>
+            <a class="nav-item" href="https://twitter.com/romanwebnet" title="Twitter" target="_blank">
+                <?php svg_icon('twitter'); ?>
+            </a>            
             <a class="nav-item" href="https://github.com/groman00" title="Github" target="_blank">
                 <?php svg_icon('github'); ?>
             </a>
-            <a class="nav-item" href="https://soundcloud.com/gr99-1" title="SoundCloud" target="_blank">
-                <?php svg_icon('soundcloud'); ?>
+            <a class="nav-item" href="https://www.linkedin.com/in/gregory-roman-8486485" title="LinkedIn" target="_blank">
+                <?php svg_icon('linkedin'); ?>
             </a>            
-            <a class="nav-item" href="mailto:groman911@gmail.com" title="E-mail">
+            <a class="nav-item" href="mailto:greg@romanwebnet.com" title="E-mail">
                 <?php svg_icon('email'); ?>
             </a>
         </nav>
--- pages/home.php
+++ pages/home.php
@@ -1,38 +1,39 @@
 <div class="home-page panel-scroller">
-
-    <section class="panel">
-        
+    <section class="panel-background">
         <div class="home-blurb">
-            <div class="img">
-                <img src="<?php echo get_template_directory_uri(); ?>/IMG_0063.JPG"/>
-            </div>
+            <div class="logo">GR</div>
             <h1>Greg Roman</h1>
             <h4>Senior Software Engineer</h4>
             <h6>New York, NY</h6>
         </div>
-
         <div class="arrow-down">
             <?php svg_icon('chevron-down', 'bounce'); ?>
         </div>
-
     </section>
-    
+    <section class="panel"></section>
     <section class="panel">
-        
         <div class="container">
-            <h2>Technologies</h2>
-            <p>lorem ipsum dolor amet</p>
+            <h2 class="panel-heading">Technologies</h2>
+            <div class="technology-grid row">
+            <?php
+                $arr = ['Javacript', 'CSS3', 'HTML5', 'jQuery', 'Page Speed', 'SEO', 'Mobile Web', 'Bootstrap', 'Node.js', 'Backbone.js', 'Gulp', 'Sass', 'PHP'];
+                foreach ($arr as &$value) {
+                    echo '<div class="col-m-6"><h4 class="title">' . $value . '</h4></div>';
+                }
+            ?>                                                             
+            </div>
         </div>
-
     </section>
-
     <section class="panel">
-        
         <div class="container">
-            <h2>Panel 3</h2>
-            <p>lorem ipsum dolor amet</p>
+            <div class="home-bio">
+                <div class="img">
+                    <img src="<?php echo get_template_directory_uri(); ?>/IMG_0063.JPG"/>
+                </div>
+                <p class="copy">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus volutpat ornare purus, accumsan venenatis lectus faucibus vitae. Sed bibendum est id hendrerit suscipit. Mauris viverra mi justo, id malesuada neque tristique id. Phasellus vel commodo massa, a mattis enim. Pellentesque sit amet tempor augue. Suspendisse dui nibh, vehicula non eros at, aliquam ultricies odio. Fusce ac pellentesque nulla. Etiam consectetur tortor quis mauris pellentesque maximus. Sed porttitor turpis non urna porttitor tincidunt.
+                </p>
+            </div>
         </div>
-
     </section>    
-
 </div>
