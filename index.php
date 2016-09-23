781788a3d12ee36f8dfb26b7f1fee70f399a6920
Roman, Greg <greg.roman@teamaol.com>
2016-09-23 10:39:27 -0400

    cleaned up breadcrumbs, article listing

--- index.php
+++ index.php
@@ -7,29 +7,21 @@ https://codex.wordpress.org/Theme_Development
 <?php get_header(); ?>
 
 	<?php if ( function_exists('yoast_breadcrumb') ) {
-  		yoast_breadcrumb('<div class="container"><div class="breadcrumbs">','</div></div>');
+  		yoast_breadcrumb('<div class="breadcrumb-container container"><div class="breadcrumbs">','</div></div>');
 	} ?>
 
-	<div class="container article-listing">
-	
-		<h1>Blog</h1>
-
-		<?php if ( have_posts() ) : ?>
-            
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'content', get_post_format() ); ?>
-
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'content', 'none' ); ?>
-
-		<?php endif; ?>
-
+	<div class="container">
+		<section class="article-listing">	
+			<h1>Blog</h1>
+			<?php if ( have_posts() ) : ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part( 'content', get_post_format() ); ?>
+				<?php endwhile; ?>
+				<?php the_posts_navigation(); ?>
+			<?php else : ?>
+				<?php get_template_part( 'content', 'none' ); ?>
+			<?php endif; ?>
+		</section>			
 	</div>
 
 <?php get_footer(); ?>
